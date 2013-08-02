bb42fd5020445eee5a60831ae445c31ee3df6200
MakerSquare Student <students@themakersquare.com>
2013-08-02 13:38:40 -0500

    Defined deliver/! method

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,9 @@ class Message < ActiveRecord::Base
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
 
   scope :unread, -> { where(read: false) }
+
+  def deliver!(sending:nil, receiving:nil)
+    raise ArgumentError, "both sender and receiver must be specified" unless sending && receiving
+
+  end
 end
